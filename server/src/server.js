0e0720a3dc6354e6c8f291813858bec7c3894f27
Divi Abdillah Almasrur <diviabdillah94@gmail.com>
2025-11-05 02:05:34 +0700

    modified:   src/server.js

--- server/src/server.js
+++ server/src/server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 5001;
 // Ganti 'https://url-frontend-vercel-anda.vercel.app' dengan URL Vercel Anda nanti
 const whitelist = [
   'http://localhost:5173', // Ganti 5173 jika port frontend lokal Anda berbeda
-  'https://url-frontend-vercel-anda.vercel.app' 
+  'sistem-manajemen-kemasan-production.up.railway.app' 
 ];
 
 const corsOptions = {
